5f71e08b219d5267e55f22a66faf7be04cbb8d08
星冉 <troy@zygd.site>
2022-05-11 12:43:39 +0800

    修改完毕

---
+++ examples/plugln/ding-ding-bot.go
@@ -0,0 +1,140 @@
+/**
+ *   Wechaty - https://github.com/wechaty/wechaty
+ *
+ *   @copyright 2020-now Wechaty
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ */
+ package main
+
+ import (
+	 "fmt"
+	 "log"
+	 "time"
+ 
+	 "github.com/wechaty/go-wechaty/wechaty"
+	 "github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
+	 "github.com/wechaty/go-wechaty/wechaty/user"
+ )
+ 
+ func main() {
+	 var bot = wechaty.NewWechaty()
+ 
+	 bot.OnScan(func(ctx *wechaty.Context, qrCode string, status schemas.ScanStatus, data string) {
+		 fmt.Printf("Scan QR Code to login: %v\nhttps://wechaty.js.org/qrcode/%s\n", status, qrCode)
+	 }).OnLogin(func(ctx *wechaty.Context, user *user.ContactSelf) {
+		 fmt.Printf("User %s logined\n", user.Name())
+	 }).OnMessage(onMessage).OnLogout(func(ctx *wechaty.Context, user *user.ContactSelf, reason string) {
+		 fmt.Printf("User %s logouted: %s\n", user, reason)
+	 })
+ 
+	 // The first plug-in
+	 bot.Use(func() *wechaty.Plugin {
+		 plugOne := wechaty.NewPlugin()
+ 
+		 // Second message processing logic
+		 plugOne.OnMessage(func(context *wechaty.Context, message *user.Message) {
+			 log.Println(message)
+ 
+			 if message.Self() {
+				 log.Println("Message discarded because its outgoing")
+			 }
+ 
+			 if message.Age() > 2*60*time.Second {
+				 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
+			 }
+ 
+			 if message.Type() != schemas.MessageTypeText || message.Text() != "plugOne" {
+				 log.Println("Message discarded because it does not match 'plugOne'")
+				 return
+			 }
+ 
+			 // 3. reply 'plugOne here!'
+			 _, err := message.Say("plugOne here!")
+			 if err != nil {
+				 log.Println(err)
+				 return
+			 }
+		 })
+		 return plugOne
+	 }())
+ 
+	 // Second plug-in
+	 bot.Use(func() *wechaty.Plugin {
+		 plugTwo := wechaty.NewPlugin()
+ 
+		 // The third message processing logic
+		 plugTwo.OnMessage(func(context *wechaty.Context, message *user.Message) {
+			 log.Println(message)
+ 
+			 if message.Self() {
+				 log.Println("Message discarded because its outgoing")
+			 }
+ 
+			 if message.Age() > 2*60*time.Second {
+				 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
+			 }
+ 
+			 if message.Type() != schemas.MessageTypeText || message.Text() != "plugTwo" {
+				 log.Println("Message discarded because it does not match 'plugTwo'")
+				 return
+			 }
+ 
+			 // 4. reply 'plugTwo here!'
+			 _, err := message.Say("plugTwo here!")
+			 if err != nil {
+				 log.Println(err)
+				 return
+			 }
+		 })
+		 return plugTwo
+	 }())
+	 bot.DaemonStart()
+ }
+ 
+ // The first message processing logic
+ func onMessage(ctx *wechaty.Context, message *user.Message) {
+	 log.Println(message)
+ 
+	 if message.Self() {
+		 log.Println("Message discarded because its outgoing")
+	 }
+ 
+	 if message.Age() > 2*60*time.Second {
+		 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
+	 }
+ 
+	 if message.Type() != schemas.MessageTypeText || message.Text() != "ding" {
+		 log.Println("Message discarded because it does not match 'ding'")
+		 return
+	 }
+ 
+	 // 1. reply 'dong'
+	 _, err := message.Say("dong")
+	 if err != nil {
+		 log.Println(err)
+		 return
+	 }
+	 log.Println("REPLY: dong")
+ 
+	 // 2. reply image(qrcode image)
+	 //fileBox, _ := file_box.FromUrl("https://wechaty.github.io/wechaty/images/bot-qr-code.png", "", nil)
+	 //_, err = message.Say(fileBox)
+	 //if err != nil {
+	 //	log.Println(err)
+	 //	return
+	 //}
+	 //log.Printf("REPLY: %s\n", fileBox)
+ }
+ 
