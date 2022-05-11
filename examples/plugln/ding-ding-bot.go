171e32ea4618e8a5c5ef9560f892eaa5f234069e
星冉 <troy@zygd.site>
2022-05-11 19:45:31 +0800

    完善 plug-in 仓库示例 :memo:

--- examples/plugln/ding-ding-bot.go
+++ examples/plugln/ding-ding-bot.go
@@ -16,125 +16,141 @@
  *   limitations under the License.
  *
  */
- package main
+package main
 
- import (
-	 "fmt"
-	 "log"
-	 "time"
- 
-	 "github.com/wechaty/go-wechaty/wechaty"
-	 "github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
-	 "github.com/wechaty/go-wechaty/wechaty/user"
- )
- 
- func main() {
-	 var bot = wechaty.NewWechaty()
- 
-	 bot.OnScan(func(ctx *wechaty.Context, qrCode string, status schemas.ScanStatus, data string) {
-		 fmt.Printf("Scan QR Code to login: %v\nhttps://wechaty.js.org/qrcode/%s\n", status, qrCode)
-	 }).OnLogin(func(ctx *wechaty.Context, user *user.ContactSelf) {
-		 fmt.Printf("User %s logined\n", user.Name())
-	 }).OnMessage(onMessage).OnLogout(func(ctx *wechaty.Context, user *user.ContactSelf, reason string) {
-		 fmt.Printf("User %s logouted: %s\n", user, reason)
-	 })
- 
-	 // The first plug-in
-	 bot.Use(func() *wechaty.Plugin {
-		 plugOne := wechaty.NewPlugin()
- 
-		 // Second message processing logic
-		 plugOne.OnMessage(func(context *wechaty.Context, message *user.Message) {
-			 log.Println(message)
- 
-			 if message.Self() {
-				 log.Println("Message discarded because its outgoing")
-			 }
- 
-			 if message.Age() > 2*60*time.Second {
-				 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
-			 }
- 
-			 if message.Type() != schemas.MessageTypeText || message.Text() != "plugOne" {
-				 log.Println("Message discarded because it does not match 'plugOne'")
-				 return
-			 }
- 
-			 // 3. reply 'plugOne here!'
-			 _, err := message.Say("plugOne here!")
-			 if err != nil {
-				 log.Println(err)
-				 return
-			 }
-		 })
-		 return plugOne
-	 }())
- 
-	 // Second plug-in
-	 bot.Use(func() *wechaty.Plugin {
-		 plugTwo := wechaty.NewPlugin()
- 
-		 // The third message processing logic
-		 plugTwo.OnMessage(func(context *wechaty.Context, message *user.Message) {
-			 log.Println(message)
- 
-			 if message.Self() {
-				 log.Println("Message discarded because its outgoing")
-			 }
- 
-			 if message.Age() > 2*60*time.Second {
-				 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
-			 }
- 
-			 if message.Type() != schemas.MessageTypeText || message.Text() != "plugTwo" {
-				 log.Println("Message discarded because it does not match 'plugTwo'")
-				 return
-			 }
- 
-			 // 4. reply 'plugTwo here!'
-			 _, err := message.Say("plugTwo here!")
-			 if err != nil {
-				 log.Println(err)
-				 return
-			 }
-		 })
-		 return plugTwo
-	 }())
-	 bot.DaemonStart()
- }
- 
- // The first message processing logic
- func onMessage(ctx *wechaty.Context, message *user.Message) {
-	 log.Println(message)
- 
-	 if message.Self() {
-		 log.Println("Message discarded because its outgoing")
-	 }
- 
-	 if message.Age() > 2*60*time.Second {
-		 log.Println("Message discarded because its TOO OLD(than 2 minutes)")
-	 }
- 
-	 if message.Type() != schemas.MessageTypeText || message.Text() != "ding" {
-		 log.Println("Message discarded because it does not match 'ding'")
-		 return
-	 }
- 
-	 // 1. reply 'dong'
-	 _, err := message.Say("dong")
-	 if err != nil {
-		 log.Println(err)
-		 return
-	 }
-	 log.Println("REPLY: dong")
- 
-	 // 2. reply image(qrcode image)
-	 //fileBox, _ := file_box.FromUrl("https://wechaty.github.io/wechaty/images/bot-qr-code.png", "", nil)
-	 //_, err = message.Say(fileBox)
-	 //if err != nil {
-	 //	log.Println(err)
-	 //	return
-	 //}
-	 //log.Printf("REPLY: %s\n", fileBox)
- }
- 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/wechaty/go-wechaty/wechaty"
+	"github.com/wechaty/go-wechaty/wechaty-puppet/schemas"
+	"github.com/wechaty/go-wechaty/wechaty/user"
+)
+
+var err error
+
+func main() {
+	var bot = wechaty.NewWechaty()
+
+	bot.OnScan(func(context *wechaty.Context, qrCode string, status schemas.ScanStatus, data string) {
+		fmt.Printf("Scan QR Code to login: %v\nhttps://wechaty.js.org/qrcode/%s\n", status, qrCode)
+	}).OnLogin(func(context *wechaty.Context, user *user.ContactSelf) {
+		fmt.Printf("User %s logined\n", user.Name())
+	}).OnLogout(func(ctx *wechaty.Context, user *user.ContactSelf, reason string) {
+		fmt.Printf("User %s logouted: %s\n", user, reason)
+	})
+
+	bot.OnMessage(onMessage)
+	// The First Plug-in
+	bot.Use(PlugOne())
+	// The Second Plug-in
+	bot.Use(PlugTwo("Hello, PlugTwo"))
+
+	bot.DaemonStart()
+}
+
+// The first onMessage processing logic
+func onMessage(context *wechaty.Context, message *user.Message) {
+	log.Println(message)
+
+	if message.Self() {
+		log.Println("Message discarded because its outgoing")
+	}
+
+	if message.Age() > 2*60*time.Second {
+		log.Println("Message discarded because its TOO OLD(than 2 minutes)")
+	}
+
+	if message.Type() != schemas.MessageTypeText || message.Text() != "ding" {
+		log.Println("Message discarded because it does not match 'ding'")
+
+		// Set needReply Status (`return`)
+		context.SetData("needReply", false)
+		return
+	}
+
+	// 1. reply 'dong'
+	if _, err = message.Say("dong"); err != nil {
+		log.Println(err)
+		return
+	}
+	log.Println("REPLY: dong")
+
+	// Set needReply Status
+	context.SetData("needReply", true)
+
+	// 2. reply image(qrcode image)
+	//fileBox, _ := file_box.FromUrl("https://wechaty.github.io/wechaty/images/bot-qr-code.png", "", nil)
+	//_, err = message.Say(fileBox)
+	//if err != nil {
+	//	log.Println(err)
+	//	return
+	//}
+	//log.Printf("REPLY: %s\n", fileBox)
+}
+
+// PlugOne (The First Plug-in)
+func PlugOne() *wechaty.Plugin {
+	newPlug := wechaty.NewPlugin()
+
+	// The Second Onmessage Processing Logic
+	newPlug.OnMessage(func(context *wechaty.Context, message *user.Message) {
+
+		// Get Value From Wechaty Context
+		needReply, ok := context.GetData("needReply").(bool)
+
+		// Determine If Parsing Error (`!`)
+		if !ok {
+			log.Println("context GetData needReply Error")
+			return
+		}
+
+		// Determine If You Have Responded (`!`)
+		if !needReply {
+			return
+		}
+
+		if _, err = message.Say("PlugOne OnMessage Here!"); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("PlugOne OnMessage Here!")
+	})
+	return newPlug
+}
+
+/*
+	PlugTwo (The Second Plug-in)
+	func PlugTwo(msg string) *wechaty.Plugin {}
+*/
+func PlugTwo(args string) *wechaty.Plugin {
+	newPlug := wechaty.NewPlugin()
+
+	// The Third Onmessage Processing Logic
+	newPlug.OnMessage(func(context *wechaty.Context, message *user.Message) {
+
+		// Get Value From Wechaty Context
+		needReply, ok := context.GetData("needReply").(bool)
+
+		// Determine If Parsing Error (`!`)
+		if !ok {
+			log.Println("context GetData needReply Error")
+			return
+		}
+
+		log.Println(args)
+
+		// Determine If You Have Responded (`!`)
+		if !needReply {
+			return
+		}
+
+		if _, err = message.Say("PlugTwo OnMessage Here!"); err != nil {
+			log.Println(err)
+			return
+		}
+		log.Println("PlugTwo OnMessage Here!")
+	})
+	return newPlug
+}
